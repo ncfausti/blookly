3378e08ca667d43494796099743dd0717455aa0e
Nick Fausti <ncfausti@gmail.com>
2015-11-17 17:25:51 -0500

    allowed pubdate to be null on stories

--- blooks/blooks/urls.py
+++ blooks/blooks/urls.py
@@ -13,33 +13,23 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import patterns,include, url
+from django.conf.urls import patterns, include, url
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from django.contrib import admin
-
+from stories.serializers import UserViewSet, StoryViewSet
 admin.autodiscover()
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'stories', StoryViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-	url(r'^api-auth/', include(router.urls)),
+	url(r'^api/', include(router.urls)),
    	url(r'^$', 'stories.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
--- blooks/stories/serializers.py
+++ blooks/stories/serializers.py
@@ -1,12 +1,28 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
 from stories.models import Story
-from blooks.urls import UserSerializer
+from django.contrib import admin
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class StorySerializer(serializers.ModelSerializer):
 	author = UserSerializer(read_only=True, required=False)
 
 	class Meta:
 		model = Story
-		fields = ('id', 'title', 'author','pub_date', 'content', 'desription')
+		fields = ('id', 'title', 'author','pub_date', 'content', 'description')
 		read_only_fields = ('id', 'pub_date')
+
+class StoryViewSet(viewsets.ModelViewSet):
+    queryset = Story.objects.all()
+    serializer_class = StorySerializer
